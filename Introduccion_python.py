0da7d22fd06f92a3e71fd60792154747d7c46b29
Antonny <110929775+antonny107846@users.noreply.github.com>
2024-10-26 02:32:05 -0600

    Add files via upload

---
+++ Introduccion_python.py
@@ -0,0 +1,14 @@
+print("hello world")
+
+
+el_codigo = "El output del codigo"
+print(el_codigo)
+
+esto_es_una_prueba = "Test Test Tes"
+print(esto_es_una_prueba)
+
+print(1+1)
+
+print(90+430)
+
+print(4*5)
