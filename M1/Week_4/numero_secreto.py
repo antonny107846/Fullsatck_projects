7c82b0bf01f7ae1976c2e06e3e2522fbac1bd0ab
Antonny <110929775+antonny107846@users.noreply.github.com>
2024-11-01 17:40:05 -0600

    Add files via upload

---
+++ M1/Week_4/numero_secreto.py
@@ -0,0 +1,25 @@
+#Modulos
+import random
+
+#Variables
+
+num_secreto = random.randint(1, 10)
+
+intentos = 5
+
+#ciclo del juego
+
+while intentos > 0:
+    numero_usuario = int(input("Adivina el número secreto (entre 1 y 10): "))
+    intentos -= 1
+
+    if numero_usuario == num_secreto:
+        print("¡Felicidades! Adivinaste el número secreto.")
+        break  # Termina el bucle si el usuario adivina
+
+    elif intentos == 0:
+        print("Te has quedado sin intentos.")
+        print(f"El número secreto era: {num_secreto}")
+    
+    else:
+        print("El número ingresado no es el número secreto, vuelva a intentarlo.")
